d8cb5f596670fed26a4e79f14ff3b4abe313721c
Macsim <134152147+MuRuLOSE@users.noreply.github.com>
2023-08-06 14:11:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
+import os
+import math
+import random
 class MOST_USELESS_CLASS:
   def output(args):
     print(args)
   def input(args):
     input(args)
+    print(args)
   def MOST_USELESS_FUNC():
     print(2+2*2+2/9) 
-MOST_USELESS_CLASS.output("some")
+  def MOST_USELESS_INFO():
+    print(os.environ,os.environ.get)
+  def MOST_MATH_INFO():
+    print(math.pi+math.e/999)  
+  def MOST_NONE_INFO():
+    print(None)
+  def MOST_TRUE_INFO():
+    print(True)
+  def MOST_FALSE_INFO():
+    print(False)
+  def MOST_MOST_INFO():
+    most = ["MOST","MUST","most","Most","mOst","moSt","mosT","MOSt"] 
+    print(random.choice(most))
+  def MOST_STUFF_THAT_ACTIVATE_ALL_OF_THIS_AND_ACTIVATE_THEMSELF():
+    MOST_USELESS_CLASS.output(None)  
+    MOST_USELESS_CLASS.input(None)
+    MOST_USELESS_CLASS.MOST_USELESS_FUNC()
+    MOST_USELESS_CLASS.MOST_USELESS_INFO()
+    MOST_USELESS_CLASS.MOST_MATH_INFO()
+    MOST_USELESS_CLASS.MOST_NONE_INFO()
+    MOST_USELESS_CLASS.MOST_TRUE_INFO()
+    MOST_USELESS_CLASS.MOST_FALSE_INFO()
+    MOST_USELESS_CLASS.MOST_MOST_INFO()
+    MOST_USELESS_CLASS.MOST_STUFF_THAT_ACTIVATE_ALL_OF_THIS_AND_ACTIVATE_THEMSELF()
