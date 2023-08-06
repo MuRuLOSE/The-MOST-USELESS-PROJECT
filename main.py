988875f8337db443e7d6c4e41bf4a380161140d9
Macsim <134152147+MuRuLOSE@users.noreply.github.com>
2023-08-06 13:55:52 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+class MOST_USELESS_CLASS:
+  def output(args):
+    print(args)
+  def input(args):
+    input(args)
+  def MOST_USELESS_FUNC():
+    print(2+2*2+2/9) 
+MOST_USELESS_CLASS.output("some")
